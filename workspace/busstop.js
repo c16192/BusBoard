72b77465b4ce0fccacf08db93de01c0697cf8bc9
Ethan Mills <ethan.mills@softwire.com>
2018-06-22 11:29:21 +0100

    listing just 5 next buses
    fixed bug with arrow function

--- workspace/busstop.js
+++ workspace/busstop.js
@@ -6,18 +6,30 @@ var BusStop = /** @class */ (function () {
         this.stopId = stopId;
     }
     BusStop.prototype.sortByTime = function (busSequence) {
+        busSequence.sort(function (a, b) {
+            return a.timeToStation - b.timeToStation;
+        });
         return busSequence;
     };
     BusStop.prototype.getBusSequence = function (num) {
+        var _this = this;
         var baseUrl = "https://api.tfl.gov.uk/StopPoint/";
         var endpoint = "/Arrivals";
         var appId = "app_id=70bf8eaf";
         var appKey = "app_key=f9015c45cebfaf6aee020c196310c4a0";
         var url = baseUrl + this.stopId + endpoint + "?" + appId + "&" + appKey;
         request(url, function (error, response, body) {
-            //let result = this.sortByTime(JSON.parse(body));
-            console.log(JSON.parse(body));
+            var allResults = _this.sortByTime(JSON.parse(body));
+            var nextBuses = _this.formatResults(allResults.slice(0, 5));
+            console.log(nextBuses);
         });
+    };
+    BusStop.prototype.formatResults = function (result) {
+        var nextBuses = [];
+        result.forEach(function (item) {
+            nextBuses.push({ line: item.lineName, timeToArrive: item.timeToStation });
+        });
+        return nextBuses;
     };
     return BusStop;
 }());
--- workspace/busstop.ts
+++ workspace/busstop.ts
@@ -1,10 +1,18 @@
 const request = require('request');
 
+interface nextBus {
+    line: string;
+    timeToArrive: number;
+}
+
 export class BusStop {
     constructor(public stopId: string){}
 
-    public sortByTime(busSequence){
-        return busSequence;
+    private sortByTime(busSequence){
+        busSequence.sort(function(a, b) {
+            return a.timeToStation - b.timeToStation;
+        })
+        return busSequence
     }
 
     public getBusSequence(num: number){
@@ -13,9 +21,21 @@ export class BusStop {
         const appId:string = "app_id=70bf8eaf";
         const appKey:string = "app_key=f9015c45cebfaf6aee020c196310c4a0"
         let url:string = baseUrl + this.stopId + endpoint + "?" + appId + "&" + appKey;
-        request(url, function (error, response, body) {
-            //let result = this.sortByTime(JSON.parse(body));
-            console.log(JSON.parse(body));
-            });
+
+
+        request(url, (error, response, body) => {
+            let allResults = this.sortByTime(JSON.parse(body))
+            const nextBuses = this.formatResults(allResults.slice(0,5));
+
+            console.log(nextBuses)
+        });
+    }
+
+    public formatResults(result: any[]): nextBus[]{
+        const nextBuses: nextBus[]= [];
+        result.forEach((item)=>{
+            nextBuses.push({line: item.lineName, timeToArrive: item.timeToStation})
+        })
+        return nextBuses;
     }
 }
