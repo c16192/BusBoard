a6faeb42d6b97907973179319ee49bc9b01ed3e1
Ethan Mills <ethan.mills@softwire.com>
2018-06-25 10:39:36 +0100

    Submitting request from form (fixed and now working)
    Now form name is set and javascript file is properly loading

--- workspace/view/fetchBuses.js
+++ workspace/view/fetchBuses.js
@@ -1,25 +1,24 @@
 function getStopData() {
-    // var url = new URL(window.location.href);
-    // let postcode = url.searchParams.get('postcode')
-    document.getElementById("content").innerHTML = "Hello!!"
-//     if (postcode == "") {return 1;}
-//     var xhttp = new XMLHttpRequest();
-//     xhttp.open("GET", "http://127.0.0.1:3000/closestStops?postcode="+postcode, false);
-//     xhttp.setRequestHeader("Content-type", "application/json");
-//     xhttp.send();
-//     var response = JSON.parse(xhttp.responseText);
-//     console.log(response)
-//     let htmlContent = "";
-//     for (item of response) {
-//         htmlContent += "<h2>"+item.busstopName+"</h2>"
-//         htmlContent += "<h2>Next Buses</h2>"
-//         htmlContent += "<table><th><td>Line Number</td><td>Time to arrive</td></th>";
-//         for (bus of item.nextBuses) {
-//             htmlContent += "<tr><td>"+ bus.line + "</td><td>"+ bus.timeToArrive +"</td></tr>";
-//         }
-//         htmlContent += "</table>"
-//     }
-//     document.getElementById("content").innerHTML = htmlContent;
-// }
+    var url = new URL(window.location.href);
+    let postcode = url.searchParams.get('postcode')
+    document.getElementById("content").innerHTML = postcode
+    if (postcode == "") {return 1;}
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "http://127.0.0.1:3000/closestStops?postcode="+postcode, false);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+    var response = JSON.parse(xhttp.responseText);
+    console.log(response)
+    let htmlContent = "";
+    for (item of response) {
+        htmlContent += "<h2>"+item.busstopName+"</h2>"
+        htmlContent += "<h2>Next Buses</h2>"
+        htmlContent += "<table><th><td>Line Number</td><td>Time to arrive</td></th>";
+        for (bus of item.nextBuses) {
+            htmlContent += "<tr><td>"+ bus.line + "</td><td>"+ bus.timeToArrive +"</td></tr>";
+        }
+        htmlContent += "</table>"
+    }
+    document.getElementById("content").innerHTML = htmlContent;
 }
 window.onload = getStopData;
